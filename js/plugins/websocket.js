655af77571e66472a4e51213675987d4f8c1f54b
teria <>
2022-02-03 02:23:16 +0900

    Revokableして残モザイク数をイベント上に表示

--- js/plugins/websocket.js
+++ js/plugins/websocket.js
@@ -2,52 +2,6 @@
 //websocket.js
 //イベントの頭上に文字を表示するプラグイン
 //===================================================================
-//Copyright (c) 2018 蔦森くいな
-//Released under the MIT license.
-//http://opensource.org/licenses/mit-license.php
-//-------------------------------------------------------------------
-//blog   : http://paradre.com/
-//Twitter: https://twitter.com/Kuina_T
-//===================================================================
-//＜更新情報＞
-//  ver1.1.0 2018/06/04 不具合修正、変数を使用可能にしました
-//  ver1.0.0 2018/01/14 初版
-//===================================================================
-
-/*:
- * @plugindesc イベントの頭上に文字を表示したい時に使います
- * @author 蔦森くいな
- *
- * @help このプラグインはイベントのページ毎に異なる設定が可能です。
- * 設定したいイベントページの実行内容１行目に
- * 「注釈」コマンドを設定し、以下のように入力します。
- *
- * info:ハロルド
- *
- * これでイベントの頭上に「ハロルド」と表示されます。
- * 文字列の中に\V[n]で変数の値を表示する事もできます。
- * また、以下のように入力すると文字の大きさを変更できます。
- *
- * info:ハロルド,40
- *
- * さらに、行を変えて以下のように入力すると
- * 文字を表示する位置を調整できます
- *
- * info:ハロルド,40
- * infoMove:50,-20
- *
- * ※infoMoveは１つ目の数字がX座標、２つ目がY座標を調整します。
- * ※infoMoveは必ずinfoより後に入力する必要があります。
- * ※行を変えずにスペースで区切ってもOKです。
- * ※仕様上、表示する文字列に「：」や「　」は含められません
- *
- *
- * 利用規約：
- * このプラグインは商用・非商用を問わず無料でご利用いただけます。
- * 使用報告やクレジット表記も必要ありません。
- * どのようなゲームに使っても、どのように加工していただいても構いません。
- * MIT Licenseにつき著作権表示とライセンスURLは残しておいて下さい。
- */
 (function() {
 const symbol = require('js/plugins/node_modules/symbol-sdk');
 const nodeUrl = 'http://sym-test-01.opening-line.jp:3000';
@@ -62,6 +16,8 @@ const raw_address = 'TDCPHIHQPN6WKJJIUCOFISJCB4NULEZOS4NCQQQ'; //revokeされる
 const address = symbol.Address.createFromRawAddress(raw_address);
 const privateKey = 'EE59AE723F6DC1BF4192997AC4D8FA2B0BBE015840ED8810AF8F2764E6D125D7';
 
+const accountHttp = repositoryFactory.createAccountRepository();
+
 
 // 指定アドレスが関与しているトランザクションが承認されたら
 // 指定アドレスのHPモザイク残高を変数にセット
@@ -69,19 +25,23 @@ const privateKey = 'EE59AE723F6DC1BF4192997AC4D8FA2B0BBE015840ED8810AF8F2764E6D1
 console.log("WebSocket前");
     listener.open().then(() => {
         listener.confirmed(address).subscribe(
-            (tx) => {
-                // console.log(block);
-                const accountHttp = repositoryFactory.createAccountRepository();
-                accountHttp.getAccountInfo(address).subscribe(
-                    // (accountInfo) => console.log(accountInfo.mosaics[5].amount.lower), // revokeする側のアドレスならとれた
-                    (accountInfo) => $gameVariables.setValue(15, accountInfo.mosaics[5].amount.lower),　// revokeする側のアドレスなら変数反映できた
-
-                    (err) => console.error(err),
-                );
-
-                // listener.close();
+            () => {
+                setHitPoint2Variable()
             },
             (err) => console.error(err),
         );
     });
+
+    function setHitPoint2Variable() {
+        let url = 'http://sym-test-02.opening-line.jp:3000/accounts/TCW5Z7YV6ZS7SIQ3ZRJMUNGUCQ6WYXNYXKV4WCY';
+        fetch(url)
+            .then(function (data) {
+                return data.json();
+            })
+            .then(function (json) {
+                console.log(json.account.mosaics[1].amount) //この参照要素をモザイクid検索から動的に
+                // 15番入れたら他プレイやにも反映されてた。オンライン共有変数だった気がするから多分それのおかげ
+                $gameVariables.setValue(15, json.account.mosaics[1].amount)
+            });
+    }
 })();
